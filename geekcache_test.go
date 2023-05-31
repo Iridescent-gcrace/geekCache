4fb3bf53d407954ba59d9251cbb3bc2cde20998a
Sagacity <781461778@qq.com>
2023-05-31 23:21:41 +0800

    单机缓存test

--- geekcache_test.go
+++ geekcache_test.go
@@ -1,6 +1,8 @@
 package geekcache
 
 import (
+	"fmt"
+	"log"
 	"reflect"
 	"testing"
 )
@@ -14,4 +16,45 @@ func TestGetter(t *testing.T) {
 	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
 		t.Errorf("callback failed")
 	}
+}
+
+// var db = map[string][string]{
+// 	"Tom":"630",
+// 	"Jack":"589",
+// 	"Sam":"555",
+// }
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+	gee := NewGroup("scores", 2<<10, GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; !ok {
+				if _, ok := loadCounts[key]; !ok {
+					loadCounts[key] = 0
+				}
+				loadCounts[key] += 1
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s no exist", key)
+		}))
+	for k, v := range db {
+		if view, err := gee.Get(k); err != nil || view.String() != v {
+			t.Fatal("failed to get value of Tom")
+		}
+		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatal("cache %s miss", k)
+		}
+	}
+
+	if view, err := gee.Get("unkown"); err != nil {
+		t.Fatal("the value of unkown should be empty but %s got", view)
+	}
+
 }
